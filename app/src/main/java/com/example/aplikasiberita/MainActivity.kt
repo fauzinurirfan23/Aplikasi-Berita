a0121632cb927542d02f817e737de6cb1cbe19d2
fauzinurirfan <fauzinurirfan4@gmail.com>
2026-01-21 17:04:15 +0700

    add layout register and main activity

--- app/src/main/java/com/example/aplikasiberita/MainActivity.kt
+++ app/src/main/java/com/example/aplikasiberita/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.aplikasiberita
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var btnRegister: Button
+    lateinit var btnLogin: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +21,19 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        btnRegister = findViewById(R.id.btn_register)
+        btnLogin = findViewById(R.id.btn_login)
+
+        btnRegister.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnLogin.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
         }
     }
 }
--- app/src/main/java/com/example/aplikasiberita/RegisterActivity.kt
+++ app/src/main/java/com/example/aplikasiberita/RegisterActivity.kt
@@ -1,12 +1,16 @@
 package com.example.aplikasiberita
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class RegisterActivity : AppCompatActivity() {
+
+    lateinit var tv_login: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +20,14 @@ class RegisterActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        tv_login = findViewById(R.id.tv_login)
+
+        tv_login.setOnClickListener {
+            val intent = Intent (this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
+
+
 }
