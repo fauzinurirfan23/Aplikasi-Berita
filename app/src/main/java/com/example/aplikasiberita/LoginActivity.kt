c638d0e7f673059aeee12431d69bd2b4ce0f4a92
fauzinurirfan <fauzinurirfan4@gmail.com>
2026-01-22 02:24:17 +0700

    add fitur login

--- app/src/main/java/com/example/aplikasiberita/LoginActivity.kt
+++ app/src/main/java/com/example/aplikasiberita/LoginActivity.kt
@@ -1,12 +1,28 @@
 package com.example.aplikasiberita
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 
 class LoginActivity : AppCompatActivity() {
+
+    lateinit var etEmail: EditText
+    lateinit var etPass: EditText
+    lateinit var btnLogin: Button
+
+    private lateinit var auth : FirebaseAuth
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +32,75 @@ class LoginActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        etEmail = findViewById(R.id.et_email)
+        etPass = findViewById(R.id.et_password)
+        btnLogin = findViewById(R.id.btn_login_process)
+
+        auth = Firebase.auth
+
+        btnLogin.setOnClickListener {
+            val email = etEmail.text.trim().toString()
+            val password = etPass.text.trim().toString()
+
+            if (email.isEmpty()){
+                etEmail.error = "Email tidak boleh kosong"
+            }
+
+            if (password.isEmpty()){
+                etPass.error = "Password tidak boleh kosong"
+            }
+
+            if (email.isNotEmpty() && password.isNotEmpty() ){
+                login(email, password)
+            }
+        }
+
     }
+
+    private fun login(email: String, password: String){
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+
+                    // Sign in success, update UI with the signed-in user's information
+
+                    val user = auth.currentUser
+                    val intent = Intent(this, HomeActivity::class.java)
+                    startActivity(intent)
+                    finish()
+
+                    Toast.makeText(
+                        baseContext,
+                        "Login Berhasil {${user?.email}",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                } else {
+
+                    Toast.makeText(
+                        baseContext,
+                        "Authentication failed.",
+                        Toast.LENGTH_SHORT,
+                    ).show()
+
+                }
+            }
+
+
+    }
+
+//    public override fun onStart() {
+//        super.onStart()
+//        // Check if user is signed in (non-null) and update UI accordingly.
+//        val currentUser = auth.currentUser
+//        if (currentUser != null) {
+//            val intent = Intent(this, MainActivity::class.java)
+//            startActivity(intent)
+//            finish()
+//
+//            //reload()
+//        }
+//    }
+
+
 }
